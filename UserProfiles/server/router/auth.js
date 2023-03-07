546bfae46a29ec0c9aee258ed873f15c2c3683f2
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-07 19:08:50 +0530

    13 Password Hash

--- UserProfiles/server/router/auth.js
+++ UserProfiles/server/router/auth.js
@@ -39,9 +39,14 @@ router.post("/register", async (req, res) => {
         if (userExist) {
             return res.status(422).json({ error: "Email already exist" });
         }
-        const user = new User({ name, email, phone, work, password });
-        await user.save()
-        res.status(201).json({ message: "User registered successfully" });
+        else if (password !== cpassword) {
+            return res.status(422).json({ error: "Passwords not matching" });
+        }
+        else {
+            const user = new User({ name, email, phone, work, password });
+            await user.save()
+            res.status(201).json({ message: "User registered successfully" });
+        }
     } catch (err) {
         console.log(err)
     }
