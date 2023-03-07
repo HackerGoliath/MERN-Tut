bbf46aaa309979e8ac0575d98975ad030e0814c7
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-07 17:38:02 +0530

    9 Storing Data with promises

--- UserProfiles/server/router/auth.js
+++ UserProfiles/server/router/auth.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
+require("../db/conn");
+const User = require("../models/userSchema");
 
 router.get("/", (req, res) => {
     res.send("Home Page Router")
 });
 
 router.post("/register", (req, res) => {
-    console.log(req.body);
-    res.json({ message: req.body });
+    const { name, email, phone, work, password } = req.body;
+    if (!name || !email || !phone || !work || !password) {
+        return res.status(422).json({ error: "Please fill the form correctly" });
+    }
+
+    User.findOne({ email: email })
+        .then((userExist) => {
+            if (userExist) {
+                return res.status(422).json({ error: "Email already exist" });
+            }
+            // const user = new User(req.body)
+            const user = new User({ name, email, phone, work, password });
+            user.save().then(() => {
+                res.status(201).json({ message: "User registered successfully" });
+            }).catch((err) => res.status(500).json({ error: "Failed to register" }))
+        }).catch(err => console.log(err))
 });
 
 
