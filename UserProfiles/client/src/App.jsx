ba7c55aeedf5e51becc1dede211f23d09896625e
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-08 17:04:15 +0530

    22 Include css

--- UserProfiles/client/src/App.jsx
+++ UserProfiles/client/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Signup from "./components/Signup"
+import Login from "./components/Login"
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
